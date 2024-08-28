0302a0fcbdac70613767f07aa0621f30f910f992
sarithasiridevops <sarithasiri.devops@example.com>
2024-08-28 17:34:34 +0530

    fruits

--- 06_array.sh
+++ 06_array.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-FRUIT=("aPP","bANANA","kiwi")
+FRUIT=("aPP" "bANANA" "kiwi")
+echo "First fruit is: ${FRUITS[0]}"
 echo "first fruit is ${FRUIT[0]}"
 echo "first fruit is ${FRUIT[1]}"
 echo "first fruit is ${FRUIT[2]}"
+
+#!/bin/bash
+
+# index starts from 0, size is 3
+#FRUITS=("APPLE" "KIWI" "ORANGE") #Array
+
+#echo "First fruit is: ${FRUITS[0]}"
+#echo "Second fruit is: ${FRUITS[1]}"
+#echo "Third fruit is: ${FRUITS[2]}"
+
+#echo "First fruit is: ${FRUITS[@]}"
